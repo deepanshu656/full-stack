dd67b0dfc6c68652259fd3927e2a41023fd264b4
deepanshu656 <deepanshukumaraiv8@gmail.com>
2024-09-12 15:29:10 +0530

    add

---
+++ javascript/loop.js
@@ -0,0 +1,24 @@
+// let i=0;
+// // condition end
+// while(i<11){
+//     console.log("hello")
+//     // update statement
+//     i++
+// }
+
+// let i=1;
+// // condition end
+// while(i<11){
+//     console.log(i)
+//     // update statement
+//     i++
+// }
+
+
+i=1;
+while(i<=100){
+     if(i%3==0 || i%5==0)
+    console.log(i)
+    i++
+    
+}
